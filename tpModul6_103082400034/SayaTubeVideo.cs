daf0e82fb3eb518cd16bac3f59d3f8832a6b5e76
virzarasydann <virzarasydan@gmail.com>
2026-04-05 19:23:29 +0700

    first commit

---
+++ tpModul6_103082400034/SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpModul6_103082400034
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            this.title = title;
+            playCount = 0;
+            Random rnd = new Random();
+            id = rnd.Next(10000, 100000);
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount = count;
+
+        }
+
+        //public void PrintVideoDetails()
+        //{
+
+        //    Console.WriteLine($"Id: {id}, Title: {title}, Play Count: {playCount}");
+        //}
+    }
+}
